d7158f0ce17647331e95b28f678f079ec5d2ad1a
NivShashoua <47760448+NivShashoua@users.noreply.github.com>
2021-09-06 18:47:58 +0300

    Update BoardGame.py

--- OctiModel/BoardGame.py
+++ OctiModel/BoardGame.py
@@ -27,10 +27,25 @@ class BoardGame:
         self.__greenArrows = 12
         self.__redArrows = 12
 
-    """ return a list of all the octs """
-    def listOfAllOct(self):
+    """ list of all the oct """
+    def __listOfAllOct(self):
         return [self.__green1, self.__green2, self.__green3, self.__green4,
                 self.__red1, self.__red2, self.__red3, self.__red4]
+
+    """ return a list of all the octs that are alive"""
+    def __listOfAllOctAlive(self):
+        allOct = self.__listOfAllOct()
+        for oct in allOct:
+            # if the oct is dead get him out of the list
+            if not oct.isAlive():
+                allOct.remove(oct)
+        return allOct
+
+    """ convert oct name to oct object """
+    def __octObject(self, oct):
+        for octObj in self.__listOfAllOct():
+            if octObj.getName() == oct:
+                return octObj
 
     """ Change the turn of the player """
     def __changeTurn(self):
@@ -41,37 +56,12 @@ class BoardGame:
 
     """ Insert an arrow in your turn """
     def insertArrow(self, oct, arrow):
-        if self.__turn == Players.Green:
-            if oct == 'G1':
-                if not self.__green1.insertArrow(arrow):
-                    return  # if there is already an arrow inside
-            elif oct == 'G2':
-                if not self.__green2.insertArrow(arrow):
-                    return
-            elif oct == 'G3':
-                if not self.__green3.insertArrow(arrow):
-                    return
-            elif oct == 'G4':
-                if not self.__green4.insertArrow(arrow):
-                    return
-            else:
-                return  # if the oct symbol was illegal, do nothing
-
-        elif self.__turn == Players.Red:
-            if oct == 'R1':
-                if not self.__red1.insertArrow(arrow):
-                    return   # if there is already an arrow inside
-            elif oct == 'R2':
-                if not self.__red2.insertArrow(arrow):
-                    return
-            elif oct == 'R3':
-                if not self.__red3.insertArrow(arrow):
-                    return
-            elif oct == 'R4':
-                if not self.__red4.insertArrow(arrow):
-                    return
-            else:
-                return  # if the oct symbol was illegal, do nothing
+        octObj = self.__octObject(oct)
+        if octObj in self.__listOfAllOctAlive() and octObj.getPlayer() == self.__turn:
+            if not octObj.insertArrow(arrow):
+                return  # if there is already an arrow inside
+        else:
+            return   # if the oct symbol was illegal or its not the player turn, do nothing
 
         # decrease one arrow to the player who used it
         if self.__turn == Players.Green:
@@ -83,77 +73,34 @@ class BoardGame:
 
     """ return the coordinates of the oct """
     def whereIs(self, oct):
-        if oct == 'G1':
-            self.__green1.getPlace()
-        elif oct == 'G2':
-            self.__green2.getPlace()
-        elif oct == 'G3':
-            self.__green3.getPlace()
-        elif oct == 'G4':
-            self.__green4.getPlace()
-        elif oct == 'R1':
-            self.__red1.getPlace()
-        elif oct == 'R2':
-            self.__red2.getPlace()
-        elif oct == 'R3':
-            self.__red3.getPlace()
-        elif oct == 'R4':
-            self.__red4.getPlace()
+        return self.__octObject().getPlace()
 
     """ What is the possible movement for an oct """
     def whereToGo(self, oct):
         # TODO: complete the function
+        octObj = self.__octObject(oct)
         arrows = []     # list of all the arrows in the wanted oct
-        if self.__turn == Players.Green:
-            if oct == 'G1':
-                octObj = self.__green1
-                arrows = self.__green1.showAllArrows()
-            elif oct == 'G2':
-                octObj = self.__green2
-                arrows = self.__green2.showAllArrows()
-            elif oct == 'G3':
-                octObj = self.__green3
-                arrows = self.__green3.showAllArrows()
-            elif oct == 'G4':
-                octObj = self.__green4
-                arrows = self.__green4.showAllArrows()
-            else:
-                return  # if the oct symbol was illegal, do nothing
 
-        elif self.__turn == Players.Red:
-            if oct == 'R1':
-                octObj = self.__red1
-                arrows = self.__red1.showAllArrows()
-            elif oct == 'R2':
-                octObj = self.__red2
-                arrows = self.__red2.showAllArrows()
-            elif oct == 'R3':
-                octObj = self.__red3
-                arrows = self.__red3.showAllArrows()
-            elif oct == 'R4':
-                octObj = self.__red4
-                arrows = self.__red4.showAllArrows()
-            else:
-                return  # if the oct symbol was illegal, do nothing
+        # check if the oct is alive an if its the player turn
+        if octObj in self.__listOfAllOctAlive() and octObj.getPlayer() == self.__turn:
+            arrows = octObj.showAllArrows()
+        else:
+            return  # if the oct symbol was illegal or it's not the right player, do nothing
+
 
         print("\narrows- ", arrows)
 
-    """ move an oct """
-    def move(self, player, oct, direction):
-        # TODO: complete the function
-        if not isinstance(direction, Directions):
-            raise TypeError('direction must be an instance of Directions Enum')
-
-        if not isinstance(player, Players):
-            raise TypeError('player must be an instance of Players Enum')
-
-        if not isinstance(oct, Oct):
-            raise TypeError('oct must be an instance of Oct class')
+    """ move an oct to a specfic cordinates"""
+    def move(self, oct, row, col):
+        location = (row, col)
+        if location in self.whereToGo(oct):
+            octObj = self.__octObject(oct)
+            octObj.setPlace(location)
 
     """ print the current state of the board """
     def printBoard(self):
         print('', end=' ')
-        allOctList = self.listOfAllOct()
+        allOctList = self.__listOfAllOctAlive()
         for row in range(self.__BOARD_LENGTH):
             for col in range(self.__BOARD_WIDTH):
                 inLocation = '--'   # what is inside this location
