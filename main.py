d938d9f3584786dc88e7777140e968302ff2a82f
NivShashoua <47760448+NivShashoua@users.noreply.github.com>
2021-09-09 03:36:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ if __name__ == '__main__':
     board = OctiModel.BoardGame.BoardGame()
     board.printBoard()
     board.insertArrow('G1', Directions.Down)
-    board.insertArrow('R2', Directions.Up)
-    board.whereToGo('G1')
+    board.insertArrow('R1', Directions.Up)
+    board.insertArrow('R1', Directions.UpLeft)
+    board.insertArrow('G2', Directions.DownLeft)
+    board.move('R1', 4, 1)
+    board.printBoard()
+    board.move('G2', 2, 1)
+    board.printBoard()
+    board.insertArrow('R3', Directions.Up)
+    board.insertArrow('G1', Directions.Right)
+    board.move('R3', 4, 3)
+    board.printBoard()
+    print(board.whereToGo('G1'))
+
+
 
 
