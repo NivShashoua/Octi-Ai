e55757fd192129daf722a57d8beb9326aebb8435
NivShashoua <47760448+NivShashoua@users.noreply.github.com>
2021-09-22 19:26:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from PyQt5.QtWidgets import *
 import OctiModel.BoardGame
 import OctiView.BoardGameView
@@ -8,25 +9,42 @@ from OctiModel.Enums import *
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     board = OctiModel.BoardGame.BoardGame()
+    view = OctiView.BoardGameView.BoardGameView(board)
+    controller = OctiController.BoardGameController.BoardGameController(board, view)
     board.printBoard()
     board.insertArrow('G1', Directions.Down)
+    view.repaint()
+    time.sleep(1)
     board.insertArrow('R1', Directions.Up)
+    view.repaint()
+    time.sleep(1)
     board.insertArrow('R1', Directions.UpLeft)
+    view.repaint()
+    time.sleep(1)
     board.insertArrow('G2', Directions.DownLeft)
+    view.repaint()
+    time.sleep(1)
     board.move('R1', 4, 1)
     board.printBoard()
+    view.repaint()
+    time.sleep(1)
     board.move('G2', 2, 1)
     board.printBoard()
+    view.repaint()
+    time.sleep(1)
     board.insertArrow('R3', Directions.Up)
+    view.repaint()
+    time.sleep(1)
     board.insertArrow('G1', Directions.Right)
+    view.repaint()
+    time.sleep(1)
     board.move('R3', 4, 3)
     board.printBoard()
     print(board.whereToGo('G1'))
-    view = OctiView.BoardGameView.BoardGameView(board)
-    print("test")
-    controller = OctiController.BoardGameController.BoardGameController(board,
-                                                                        view)
     sys.exit(app.exec_())
+    print("nnn")
+
+
 
 
 
