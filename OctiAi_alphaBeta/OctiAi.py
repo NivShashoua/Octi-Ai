7fb0f2b90ed1e0c164397649163c4e963d3f85da
NivShashoua <47760448+NivShashoua@users.noreply.github.com>
2021-10-31 17:49:49 +0200

    Create OctiAi.py

---
+++ OctiAi_alphaBeta/OctiAi.py
@@ -0,0 +1,16 @@
+import json
+
+class OctiAi:
+
+    def __init__(self, board):
+        self.__AiBoard = board
+
+    def __alpha(self):
+        """TODO: niv"""
+
+    def __beta(self):
+        """TODO: niv"""
+
+
+    def __evaluation(self,jsonBoard):
+        """TODO: together"""
